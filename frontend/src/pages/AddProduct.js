e6961eb89b8491fc50a10e6e765408a793a7fd0f
Khadija Rafaqat <khadijarafaqat2@gmail.com>
2026-07-24 00:17:49 +0500

    Added AddProduct Feature

--- frontend/src/pages/AddProduct.js
+++ frontend/src/pages/AddProduct.js
@@ -1,7 +1,92 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addProduct } from "../services/productService";
+
+
 function AddProduct() {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [company, setCompany] = useState('');
+    const navigate = useNavigate();
+
+    const handleAddProduct = async (e) =>{
+        e.preventDefault();
+
+        if (!name || !price || !category || !company){
+            alert("Please fill all fields.");
+            return;
+        }
+
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        const response = await addProduct({
+            name,
+            price,
+            category,
+            company,
+            userId: user.user._id,
+        });
+
+        if (response.ok){
+            alert("Product added successfully");
+            navigate('/');
+        }else{
+            alert(response.data.message);
+        }
+    }
+
+
     return (
-        <h1>Add Product Page</h1>
-    );
+        <div className="register-container">
+            <div className="register-card">
+                <h1>Add Product</h1>
+                <p>Add a new product</p>
+
+                <form onSubmit={handleAddProduct}>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            placeholder="Enter Product Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <input
+                            type="number"
+                            placeholder="Enter Product Price"
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            placeholder="Enter Product Category"
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            placeholder="Enter Company Name"
+                            value={company}
+                            onChange={(e) => setCompany(e.target.value)}
+                        />
+                    </div>
+
+                    <button type="submit">Add Product</button>
+                </form>
+            </div>
+        </div>
+
+    )
+
 }
 
 export default AddProduct;
--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,8 +1,23 @@
-// import { API_URL } from "./config";
+import { API_URL } from "./config";
 
 export async function getProducts() {}
 
-export async function addProduct() {}
+export async function addProduct(productData) {
+    const response = await fetch (`${API_URL}/add-product`,{
+        method:"POST",
+        headers:{
+            "Content-Type":'application/json',
+        },
+        body: JSON.stringify(productData),
+    });
+
+    const result = await response.json();
+    return{
+        ok: response.ok,
+        data:result,
+    };
+
+}
 
 export async function updateProduct() {}
 
